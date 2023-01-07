8e784ff0a76dd882811c137c71c737d37f7779f8
Caleb V <91645065+Aviator2276@users.noreply.github.com>
2023-01-07 14:06:27 +0000

    Added security feature

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+passw = input(str("Please set a password: "))
 print("Hello World")
 i = input(str("This is a test"))
 print(i)
+login = input(str("Please enter your password: "))
+if (login == passw):
+  print("Correct")
+else:
+  print("Incorrect")
