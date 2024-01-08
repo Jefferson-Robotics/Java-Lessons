218877125717c5556d1049e1eb65e2b0889b9dab
aviator2277 <aviator2276@gmail.com>
2024-01-08 23:53:44 +0000

    Added assignment 1

---
+++ Unit 2 - Java/Chapter 1 - Introduction to Java/Basic Bot /Main.java
@@ -0,0 +1,12 @@
+class Main {
+    int[][] arrayMap = new int[][] {
+        { 0, 0, 0, 0, 0},
+        { 0, 0, 0, 1, 0},
+        { 0, 0, 0, 0, 0},
+        { 0, 0, 0, 0, 0},
+        { 0, 0, 0, 0, 0}
+      };
+    public static void main(String[] args) {
+        
+    }
+}
